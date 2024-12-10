b864c749b008ee3c3c41955228380b58d9091874
Rafaela Rodrigues <rafaela@highnote.io>
2024-12-09 21:44:38 -0300

    Update Terraform provider requirements and version in provider configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.80.0"
+    }
+  }
   backend "s3" {
     bucket = "fiap-tech-challenge-terraform"
     key    = "fiap-tech-challenge-terraform-eks/terraform.tfstate"
