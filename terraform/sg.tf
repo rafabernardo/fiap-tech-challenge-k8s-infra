493cfb9e39ac062b30a201c233cf371447fb0a7e
Alceu Ramos <alceu.rcjr@gmail.com>
2024-12-10 22:53:50 -0300

    feat: Add security group rules

---
+++ terraform/sg.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "sg" {
+  name        = "SG-${var.eksName}"
+  description = "Este grupo e usado na live de terraform"
+  vpc_id      = data.aws_vpcs.selected.ids[0]
+
+  ingress {
+    description = "All"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    description = "All"
+    from_port   = 5050
+    to_port     = 5050
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
